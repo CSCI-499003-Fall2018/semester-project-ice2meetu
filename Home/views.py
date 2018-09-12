c5456d306f4413563ac9a61dd987550ca2150039
KevinCybura <KevinCybura@gmail.com>
2018-09-12 16:02:35 -0400

    More boiler plate

--- Home/views.py
+++ Home/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+creators = [
+    {
+        'name' : 'Kevin',
+        'school': 'Hunter College'
+    },
+    {
+        'name' : 'Dandan',
+        'school': 'Hunter College'
+    },
+    {
+        'name' :'Silvena',
+        'school': 'Hunter College'
+    },
+    {
+        'name': 'Kristoff',
+        'school': 'Hunter College'
+    }
+]
 
 def home(request):
-    return render(request, 'Home/home.html')
+    context = {
+        'creators': creators,
+        'title': 'IceToMeetYou'
+    }
+    return render(request, 'Home/home.html', context)
