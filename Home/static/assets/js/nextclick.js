f124909bda20c979e0f57343f26aefb5c691762e
Silvena Chan <Silvena.Chan41@myhunter.cuny.edu>
2018-09-24 20:17:39 -0400

    added simple flask game api + html page for it, fixed navbar paths with url names

---
+++ Home/static/assets/js/nextclick.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('#next').onclick = function() {
+    location.reload();
+  }
+});
--- Home/urls.py
+++ Home/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home_creation_page"),
     path('signup', views.signup, name="sign_up_page"),
+    path('game', views.game, name="game")
 ]
