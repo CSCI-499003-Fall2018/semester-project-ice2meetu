09a4af58513f8071342b6d0efb5f2aa3288c643a
silvena <silvena@umich.edu>
2018-09-28 10:47:23 -0400

    changed games to app with db

---
+++ games/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class GameType(models.Model):
+    types_list = (
+        ('WUR', 'Would You Rather...?'),
+        ('TOD', 'Truth or Dare'),
+        ('ACT', 'Activity'),
+        ('DIS', 'Discuss'),
+        ('OTH', 'Other')
+    )
+    game_type = models.CharField(max_length=3, choices=types_list)
+    num_players = models.IntegerField()
+
+    def __str__(self):
+        return "{}: {} ({} Players)".format(self.game_type,
+                                            self.get_game_type_display(),
+                                            self.num_players)
+
+class Game(models.Model):
+    game_type = models.ForeignKey(GameType, default="",
+                             on_delete=models.CASCADE)
+    text = models.TextField(blank=True)
+
+    def __str__(self):
+        type_str = self.game_type.get_game_type_display()
+        return "{}: {}".format(type_str, self.text)
