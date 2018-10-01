19cda5be70ea19bc494d182f4657dc3494af0156
silvena <silvena@umich.edu>
2018-10-01 16:47:51 -0400

    added game route function

--- Home/views.py
+++ Home/views.py
@@ -47,23 +47,22 @@ def signup(request):
             return redirect('Home')
     else:
         form = UserCreationForm()
-    return render(request, 'Home/signup.html', {'form': form})    
+    return render(request, 'Home/signup.html', {'form': form})
 
 def login(request):
     return render(request, 'Home/login.html', {})
 
 
-def game(request):
-    tunnel = "3d0b7810"
-
-    def get_game():
-        url = "http://{}.ngrok.io/".format(tunnel)
-        response = requests.get(url)
-        if response.status_code != 200:
-            return {}
-        return response.json()
-
-    context = get_game()
+def game(request, nplayers=None):
+    if not nplayers:
+        return render(request, 'Home/game.html', {'selected': False})
+    filtered_games = Game.objects.filter(game_type__num_players__exact=nplayers)
+    rand_game = random.choice(filtered_games)
+    context = {
+        'text': rand_game.text,
+        'game': rand_game.game_type.get_game_type_display(),
+        'selected': True
+    }
     return render(request, 'Home/game.html', context)
 
 
