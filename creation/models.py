c3bfde9dfdd5330d8cd90591415603c6eab45d31
kristoff-campbell28 <kristoff.campbell28@gmail.com>
2018-10-10 15:40:53 -0400

    Adding tentative Groups code.

--- creation/models.py
+++ creation/models.py
@@ -1,14 +1,6 @@
 from django.db import models
 from django.utils import timezone
 
-class Blog(models.Model):
-    title = models.CharField(max_length=255, default='', blank=True)
-
-    def __str__(self):
-        return "{}".format(self.title)
-
-class User(models.Model):
-    name = models.CharField(max_length=255)
 
 class Event(models.Model):
     title = models.CharField(max_length=255)
@@ -16,9 +8,16 @@ class Event(models.Model):
     admin = models.CharField(max_length=100);
     description = models.CharField(default="", max_length=500)
     event_type = models.CharField(max_length=100)
-    users = models.ManyToManyField(User, related_name="event_user")
+    # users = models.ManyToManyField(User, related_name="event_user")
     created_date = models.DateTimeField(
             default=timezone.now) 
     access_code = models.CharField(max_length=8)
+
+class Group(models.Model):
+	event = models.ForeignKey(Event, )
+	
+class User(models.Model):
+	event = models.ForeignKey(Event, )
+    name = models.CharField(max_length=255)
 
 # Create your models here.
