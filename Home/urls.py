e955d3e60ed1bb23b4b44dfd3af79b0218d7fc58
Dandan Lin <dandanlin.l@gmail.com>
2018-10-15 16:31:38 -0400

    Minor Error

--- Home/urls.py
+++ Home/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # path('logout/', auth_views.LogoutView.as_view, {'next_page': '/'}, name='logout_page'),
     path('logout/', auth_views.LogoutView.as_view(template_name="/"), name='logout_page'),
     path('signup', views.signup, name="sign_up_page"),
-    path('profile', views.profile, name="home_creation_page"),
+    path('profile', views.profile, name="profile_page"),
     path('game', views.game, name="game"),
     path('get_game', views.get_nplayer_game, name="get_game"),
     path('join', views.join, name="join_page")
