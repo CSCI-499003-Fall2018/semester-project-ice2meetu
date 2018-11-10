fb4ed296d63e24ab53d38dbac486b769229f7099
Silvena Chan <silvenac@users.noreply.github.com>
2018-11-10 13:27:47 -0500

    new models + admin site customizations

--- creation/models.py
+++ creation/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 import random
+from games.models import Game, GameType
 
 class Event(models.Model):
     title = models.CharField(max_length=255)
