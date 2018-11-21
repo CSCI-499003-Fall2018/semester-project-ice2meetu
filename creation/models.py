8c687d45f872a9c1b22679bc7affda590d993070
Silvena Chan <silvenac@users.noreply.github.com>
2018-11-21 15:25:10 -0500

    Event play (#28)
    * new models + admin site customizations
    
    * models, utils ready. need to adjust grping for some kind of event trigger
    
    * intermediate commit, nonworking
    
    * random grouping works, need to persist state and create trigger to play
    
    * play trigger test + render group game for user (no error handling yet)
    
    * bug fixes in db queries
    
    * change template inheritance to Home/base.html

--- creation/models.py
+++ creation/models.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
+from games.models import Game, GameType
 import random
+from games.models import Game, GameType
 
 class Event(models.Model):
     title = models.CharField(max_length=255)
@@ -11,11 +13,12 @@ class Event(models.Model):
     created_date = models.DateTimeField(
         default=timezone.now)
     access_code = models.CharField(max_length=8)
+    is_playing = models.BooleanField(default=False)
 
     def user_count(self):
-        try:
+        if self.event_users.exists():
             return self.event_users.count()
-        except AttributeError:
+        else:
             return 0
     
     def has_current_grouping(self):
@@ -43,7 +46,7 @@ class Event(models.Model):
     def save_group_history(self):
         curr_groups = self.grouping_set.filter(is_current=True)
         if not curr_groups.exists():
-            raise AttributeError("Error: No Groupings to save")
+            raise RuntimeError("Error: No Groupings to save")
 
         past_groups = self.get_grouping_hist() 
         for group in list(curr_groups)[0].groups():
@@ -87,6 +90,31 @@ class Grouping(models.Model):
 class Group(models.Model):
     max_size = models.IntegerField(default=9)
     grouping = models.ForeignKey(Grouping, on_delete=models.CASCADE, null=True)
+    is_complete = models.BooleanField(default=False)
+    game = models.ForeignKey(Game, on_delete=models.DO_NOTHING, null=True, blank=True)
+
+    @property
+    def is_playing(self):
+        return self.event().is_playing
+
+    def size(self):
+        if self.eventuser_set.exists():
+            return self.eventuser_set.count()
+        else:
+            return 0
+    
+    def users(self):
+        return {user for user in self.eventuser_set.all()}
+    
+    def event(self):
+        return grouping.event
+    
+    def __eq__(self, other):
+        if self.size() != other.size():
+            return False
+        if isinstance(other, Group):
+            return self.users() == other.users()
+        return False
 
     def size(self):
         if self.eventuser_set.exists():
@@ -115,6 +143,18 @@ class EventUser(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     events = models.ManyToManyField(Event, related_name='event_users', blank=True)
     groups = models.ManyToManyField(Group, blank=True)
+
+    def is_playing(self):
+        return self.events.filter(is_playing=True).exists()
+    
+    def current_game(self):
+        event = self.events.filter(is_playing=True)[0]
+        groupings = event.grouping_set.filter(is_current=True)[0]
+        group = None
+        for g in groupings.groups():
+            if g in self.groups.all():
+                group = g
+        return group.game
 
     def __str__(self):
         return "{}".format(self.user.username)
