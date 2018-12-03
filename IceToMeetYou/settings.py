8cc8dd1d2148e824835a369105759fcccd2dacf9
KevinCybura <KevinCybura@gmail.com>
2018-12-03 15:42:05 -0500

    Auth Token Done

--- IceToMeetYou/settings.py
+++ IceToMeetYou/settings.py
@@ -168,7 +168,10 @@ LOGOUT_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
 }
