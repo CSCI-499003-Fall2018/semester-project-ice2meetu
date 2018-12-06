7a53ff2df0d3a223f25d2e8750f3862eb163d585
Dandan Lin <dandanlin.l@gmail.com>
2018-12-05 20:07:45 -0500

    Adding group

--- IceToMeetYou/settings.py
+++ IceToMeetYou/settings.py
@@ -76,7 +76,7 @@ ROOT_URLCONF = 'IceToMeetYou.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['group/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- group/views.py
+++ group/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-from .models import Game, GameType
 from django.contrib.auth.decorators import login_required
 import random
 from django.http import HttpResponseRedirect, JsonResponse
 
 @login_required(login_url='login/')
 def same_group(request):
-    return render(request, "group/group.html")
+    def get_group_id(request):
+        if not request.user.eventuser_set.exists():
+            return None
+        user = request.user.eventuser_set.all()[0]
+        event = user.player.game_manager.event
+        grouping = event.game_manager.get_current_grouping()
+
+        for group in grouping.groups():
+            if user in group.users():
+                group_id = int('0' + group.pk)
+                return group_id
+    
+        return None
+
+    context = {
+        "group_id": get_group_id(request)
+    }
+
+    return render(request, "group/group.html", context)
