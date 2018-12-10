da608272af296f0dcf16ac15a1678875ed96f0bb
Silvena Chan <silvenac@users.noreply.github.com>
2018-12-10 00:41:27 -0500

    group sockets now async

--- group/consumers.py
+++ group/consumers.py
@@ -1,44 +1,52 @@
 from creation.models import Group
-from asgiref.sync import async_to_sync, sync_to_async
-from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
+from channels.db import database_sync_to_async
+from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
 
-class GroupConsumer(WebsocketConsumer):
-    def connect(self):
+class GroupConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         user = self.scope['user']
-        eventuser = user.eventuser_set.all()[0]
-        group = eventuser.current_group()
+        eventuser = await database_sync_to_async(self.get_user)(user)
+        group = await database_sync_to_async(eventuser.current_group)()
         self.group_id = str(group.pk)
 
         # Join group
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.group_id,
             self.channel_name
         )
 
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
+    def get_user(self, user):
+        return user.eventuser_set.all()[0]
+
+    async def disconnect(self, close_code):
         # Leave group connection
-        async_to_sync(self.channel_layer.group_discard)(
+        await self.channel_layer.group_discard(
             self.group_id,
             self.channel_name
         )
+    
+    @database_sync_to_async
+    def set_group_complete(self):
+        group = Group.objects.get(pk=int(self.group_id))
+        if not group.is_complete:
+            group.is_complete = True
+            group.save()
 
     # Receive message from WebSocket
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         complete = text_data_json['complete']
 
         if complete:
-            group = Group.objects.get(pk=self.group_id)
-            if not group.is_complete:
-                group.is_complete = True
-                group.save()
+            await self.set_group_complete()
 
         # Send message to group
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.group_id,
             {
                 'type': 'notify',
@@ -47,10 +55,10 @@ class GroupConsumer(WebsocketConsumer):
         )
 
     # Receive message from group
-    def notify(self, event):
+    async def notify(self, event):
         complete = event['complete']
 
         # Send message to WebSocket
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'complete': True
         }))
