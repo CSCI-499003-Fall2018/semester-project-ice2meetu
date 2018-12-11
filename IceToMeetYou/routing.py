f85d3d65ef2306314270c9c9f200d92583eac9e9
Dandan Lin <dandanlin.l@gmail.com>
2018-12-11 18:28:03 -0500

    Fonts

--- IceToMeetYou/routing.py
+++ IceToMeetYou/routing.py
@@ -2,7 +2,7 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path, include
 import group.routing
-import event.routing
+# import event.routing
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
