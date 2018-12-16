3f18d41533d49094e7d1c3270200f78d1944446f
Dandan Lin <Dandanlin0702@users.noreply.github.com>
2018-12-16 00:07:17 -0500

    Various Changes (#50)
    * Fonts
    
    * Secure Route
    
    * Testing
    
    * Done
    
    * Remove Unnecessary Changes

--- IceToMeetYou/routing.py
+++ IceToMeetYou/routing.py
@@ -2,6 +2,7 @@ from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path, include
+
 from django.conf.urls import url
 from group.consumers import GroupConsumer
 from event.consumers import EventConsumer
